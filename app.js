4c78a04ca87669dc7a2714484b19ef859b2a9cec
joseph walter <joeywalter@josephs-MacBook-Pro.local>
2018-10-08 17:14:58 -0600

    added use bodyparser and cors

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(port, () => {
+    console.log(`Port ${port} is now Active!`);
+});
+
+app.get('/', (req, res, next) =>   {
+})
