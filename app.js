c37a0c915788b59dc055142e21023f46a17f3a86
joseph walter <joeywalter@josephs-MacBook-Pro.local>
2018-10-09 15:55:14 -0600

    still working on post and put, but get is working-ish

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const queries = require('./queries');
+
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -12,4 +14,23 @@ app.listen(port, () => {
 });
 
 app.get('/', (req, res, next) =>   {
-})
+    queries.allAuthors().then(result => { res.send({data: result})})
+});
+
+app.get('/:id', (req,res) => {
+    queries.indiBooks(req.params.id).then(result => { res.send({data: result})})
+} );
+
+app.get('/:id/:id', (req,res) => {
+    queries.indiAuthors(req.params.id).then(result => { res.send({data: result})})
+} );
+
+app.post('/', (req, res) => {
+    queries.createAuthor(req.body).then((data) => {
+    res.json({data});
+    });
+});
+
+app.put('/:id', (req,res) =>   {
+    queries.updateBooks(req.body, req.params.id).then(itemData => res.json({data: itemData}))
+});
